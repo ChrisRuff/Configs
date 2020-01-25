8735e27e3199446a1b7dccf82c1745ef4689b667
chrisruff <Chrisruff@live.ca>
2020-01-25 12:14:51 -0400

    Added downloading packages and setting defaults

--- setup.sh
+++ setup.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
-echo "Creating Symlinks"
-ln -sfn $PWD/Vim/vimrc /etc/vimrc
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~Downloading Packages"
+pacman --noconfirm -S yay
+
+yay --noconfirm -S redshift python spotify google-chrome \
+vim vifm fontforge gitkraken
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~Creating Symlinks"
+ln -sfn $PWD/Vim/vimrc ~/.vimrc
 ln -sfn $PWD/i3/config ~/.i3/config
 ln -sfn $PWD/i3/i3_shell.sh ~/.i3/i3_shell.sh
 ln -sfn $PWD/Vifm/vifmrc ~/.config/vifm/vifmrc
 
 
-echo "Updating Bash Profile"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~Updating Bash Profile"
 cat $PWD/Bashrc_Additions/bashrc >> ~/.bashrc
+
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~Setting default programs"
+ln -sfn $PWD/Defautls/.profile ~/.profile
+ln -sfn $PWD/Defaults/mimeapps.list ~/.configs/mimeapps.list
