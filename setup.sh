13147cd5f80d829468ee75675479f5827ec842df
Chris Ruff <Chrisruff@live.ca>
2020-03-12 20:59:21 -0300

    Added Ranger, Redshift, and Vimb

--- setup.sh
+++ setup.sh
@@ -4,22 +4,32 @@ echo "~~~~~~~~~~~~~~~~~~~~~~~~~~Downloading Packages"
 pacman --noconfirm -S yay
 
 yay --noconfirm -S redshift python spotify google-chrome \
-vim vifm fontforge gitkraken
+gvim ranger fontforge gitkraken vimb-git
 
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~Creating Symlinks"
+
 ln -sfn $PWD/Vim/vimrc ~/.vimrc
+
+mkdir -p ~/.i3
 ln -sfn $PWD/i3/config ~/.i3/config
 ln -sfn $PWD/i3/i3_shell.sh ~/.i3/i3_shell.sh
-ln -sfn $PWD/Vifm/vifmrc ~/.config/vifm/vifmrc
 
+mkdir -p ~/.config/vimb
+ln -sfn $PWD/Vimb/config ~/.config/vimb/config
+ln -sfn $PWD/Vimb/style.css ~/.config/vimb/style.css
+
+mkdir -p ~/.config/ranger
+ln -sfn $PWD/Ranger/rc.conf ~/.config/ranger/rc.conf
+
+mkdir -p ~/.config/redshift
+ln -sfn $PWD/Redshift/redshift.conf ~/.config/redshift/redshift.conf
 
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~Updating Bash Profile"
-cat $PWD/Bashrc_Additions/bashrc >> ~/.bashrc
-
+#cat $PWD/Bashrc_Additions/bashrc >> ~/.bashrc
 
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~Setting default programs"
 ln -sfn $PWD/Defautls/.profile ~/.profile
 ln -sfn $PWD/Defaults/mimeapps.list ~/.configs/mimeapps.list
 
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~Building Vim Packages"
-python3 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
+#python3 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
