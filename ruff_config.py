f4b85be33621170cb9057b302eb5157957b91bfc
Chris <ChrisRuff@live.ca>
2025-04-30 14:22:30 -0300

    Done use hard links, since they dont work right

--- ruff_config.py
+++ ruff_config.py
@@ -1,9 +1,29 @@
 import os
 import subprocess
 import argparse
+import shutil
 
 packages = ["neovim", "neofetch", "kitty"]
 root = os.path.dirname(__file__)
+
+
+config_map = {
+    "nvim": "nvim",
+    "i3": "i3",
+    "ranger": "ranger",
+    "redshift": "redshift",
+    "gtk": "gtk-3.0",
+    "kitty": "kitty",
+    "btop": "btop",
+    "clamav": "clamav",
+    "code": "Code",
+    "hypr": "hypr",
+    "mangohud": "MangoHud",
+    "fish": "fish",
+    "neofetch": "neofetch",
+    "neovide": "neovide",
+    "waybar": "waybar",
+}
 
 def print_header(title):
     print("#########", title, "#########")
@@ -33,28 +53,16 @@ def check_for_mismatch(config_path, sys_path):
         
     return content1 != content2
 
-def are_hardlinked(f1, f2):
-    if not (os.path.isfile(f1) and os.path.isfile(f2)):
-        return False
-    return os.path.samefile(f1, f2) or (os.stat(f1).st_ino == os.stat(f2).st_ino)
-
 def place_config(config_path, sys_path, overwrite=False, dry_run=False):
     if os.path.exists(sys_path):
-        if not are_hardlinked(config_path, sys_path) and overwrite:
-            if check_for_mismatch(config_path, sys_path):
-                print_header("CONFIG MISMATCH")
-                print("Mismatch", config_path, "and", sys_path, ", please sync them and rerun this script")
-                return
-            print("Removing", sys_path)
-            if not dry_run:
-                os.remove(sys_path)
-        else:
-            print("Skipping", sys_path)
+        if check_for_mismatch(config_path, sys_path):
+            print_header("CONFIG MISMATCH")
+            print("Mismatch", config_path, "and", sys_path, ", please sync them and rerun this script")
             return
-
-    print(config_path, "->", sys_path)
-    if not dry_run:
-        os.link(get_path(config_path), expand_home(sys_path))
+    else:
+        print(config_path, "->", sys_path)
+        if not dry_run:
+            shutil.copy(config_path, sys_path)
 
 def place_configs(config_path, sys_path, overwrite=False, dry_run=False):
     os.makedirs(sys_path, exist_ok=True)
@@ -80,32 +88,42 @@ def download(dry_run=False):
 
 def links(overwrite=False, dry_run=False):
     print_header("Creating Symlinks")
-    place_configs("nvim", expand_home(".config/nvim"), overwrite, dry_run)
-    place_configs("i3", expand_home(".config/i3"), overwrite, dry_run)
-    place_configs("ranger", expand_home(".config/ranger"), overwrite, dry_run)
-    place_configs("redshift", expand_home(".config/redshift"), overwrite, dry_run)
-    place_configs("intellij", expand_home(""), overwrite, dry_run)
-    place_configs("bash", expand_home(""), overwrite, dry_run)
-    place_configs("xresources", expand_home(""), overwrite, dry_run)
-    place_configs("picom", expand_home(".config"), overwrite, dry_run)
-    place_configs("gtk", expand_home(".config/gtk-3.0"), overwrite, dry_run)
-    place_configs("kitty", expand_home(".config/kitty"), overwrite, dry_run)
-    place_configs("latex", expand_home(""), overwrite, dry_run)
-    place_configs("git", expand_home(""), overwrite, dry_run)
-    place_configs("btop", expand_home(".config/btop"), overwrite, dry_run)
-    place_configs("clamav", expand_home(".config/clamav"), overwrite, dry_run)
-    place_configs("code", expand_home(".config/Code"), overwrite, dry_run)
-    place_configs("hypr", expand_home(".config/hypr"), overwrite, dry_run)
-    place_configs("mangohud", expand_home(".config/MangoHud"), overwrite, dry_run)
-    place_configs("fish", expand_home(".config/fish"), overwrite, dry_run)
-    place_configs("neofetch", expand_home(".config/neofetch"), overwrite, dry_run)
-    place_configs("neovide", expand_home(".config/neovide"), overwrite, dry_run)
-    place_configs("waybar", expand_home(".config/waybar"), overwrite, dry_run)
+    for item, path in config_map.items():
+        place_configs(item, expand_home(f".config/{path}"), overwrite, dry_run)
 
     os.makedirs(expand_home(".config/i3status"), exist_ok=True)
     place_config(get_path("i3/i3status.conf"), expand_home(".config/i3status/config"), overwrite, dry_run)
 
+def sync_config(sys_path, config_path):
+    if not os.path.exists(sys_path):
+        print_header("SYSTEM CONFIG DOES NOT EXIST")
+        print("Missing", sys_path)
+        return
+    elif os.path.exists(config_path):
+        if check_for_mismatch(config_path, sys_path):
+            print("Syncing:", sys_path, "->", config_path)
 
+            os.remove(config_path)
+            shutil.copy(sys_path, config_path)
+    else:
+        print_header("THIS REPO'S CONFIG DOES NOT EXIST")
+        print("Missing", config_path)
+        return
+
+def sync_configs(sys_path, config_path):
+    files = get_files(get_path(config_path))
+    for file in files:
+        dir = [os.path.join(sys_path,file),
+               get_path(os.path.join(config_path, file))]
+        sync_config(dir[0], dir[1])
+
+def sync():
+    print_header("Syncing Configs")
+    for item, path in config_map.items():
+        sync_configs(expand_home(f".config/{path}"), item)
+
+    sync_config(expand_home(".config/i3status/config"), "i3/i3status.conf")
+    print_header("Done Syncing Configs")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Configure system settings and create symlinks')
@@ -113,11 +131,17 @@ if __name__ == "__main__":
                         help='Overwrite existing config files')
     parser.add_argument('--download', '-d', action='store_true', default=False,
                         help='Download and install packages')
+    parser.add_argument('--sync', '-s', action='store_true', default=False,
+                        help='Sync config files with system')
     parser.add_argument('--dry-run', '-n', action='store_true', default=False,
                         help='Do not create symlinks or download packages, just print what would be done')
     args = parser.parse_args()
-    
-    if args.download:
-        download(args.dry_run)
-    
-    links(args.overwrite, args.dry_run)
+
+    if args.sync:
+        sync()
+
+    else:
+        if args.download:
+            download(args.dry_run)
+        
+        links(args.overwrite, args.dry_run)
